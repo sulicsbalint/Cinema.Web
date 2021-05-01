a6140f7383a939021417ae83c895e4d34e7c1936
Sulics Bálint <sulicsbalint@gmail.com>
2021-05-01 13:39:29 +0200

    Added WebApi project to solution

---
+++ Cinema.WebApi/Startup.cs
@@ -0,0 +1,85 @@
+using Cinema.Persistence;
+using Cinema.Persistence.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cinema.WebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            DbType dbType = Configuration.GetValue<DbType>("DbType");
+
+            switch (dbType)
+            {
+                case DbType.SqlServer:
+                    services.AddDbContext<CinemaDbContext>(options =>
+                    {
+                        options.UseSqlServer(Configuration.GetConnectionString("SqlServerConnection"));
+                    });
+                    break;
+                case DbType.Sqlite:
+                    services.AddDbContext<CinemaDbContext>(options =>
+                    {
+                        options.UseSqlServer(Configuration.GetConnectionString("SqliteConnection"));
+                    });
+                    break;
+            }
+
+            services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 3;
+                options.Password.RequiredUniqueChars = 0;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+            })
+            .AddEntityFrameworkStores<CinemaDbContext>()
+            .AddDefaultTokenProviders();
+
+            services.AddTransient<ICinemaService, CinemaService>();
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
